0034e9c51bad89a41e5346355e340bb446d9265e
buckethead1986 <rspeckpfs@gmail.com>
2017-10-10 18:53:55 -0400

    Done.

---
+++ app/models/superhero.rb
@@ -0,0 +1,17 @@
+class Superhero
+  attr_accessor :name, :power, :bio
+
+  @@heroes = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    @@heroes << self
+  end
+
+  def self.all
+    @@heroes
+  end
+
+end
